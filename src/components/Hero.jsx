419a0bb23a96b58c533c949aaeed7e948ff17894
Exequiel Lobaiza <exelob25@gmail.com>
2024-09-12 08:54:50 -0300

    copy change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero(){
     return(
         <header className="hero">
             <img src="src/assets/header.png"/>
-            <h1>Online Experiences</h1>
+            <h1>Online Experiencess</h1>
             <h2>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h2>
         </header>
     )

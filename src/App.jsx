e03cdc54dc8cfe01745b3621a94345e2fa3c8814
Exequiel Lobaiza <exelob25@gmail.com>
2024-09-17 14:27:32 -0300

    change properties names

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,15 @@ import Navbar from './components/navBar'
 import data from './data.js'
 
 function App() {
-  const experiencesOnline = data.map(datas => { 
+  const experiencesOnline = data.map(item => { 
     return <Card
-        img = {datas.coverImg}
-        rating = {datas.stats.rating}
-        reviewCount = {datas.stats.reviewCount}
-        location = {datas.location}
-        title = {datas.title}
-        price = {datas.price}
-        cost ={datas.const}
+        img = {item.coverImg}
+        rating = {item.stats.rating}
+        reviewCount = {item.stats.reviewCount}
+        location = {item.location}
+        title = {item.title}
+        price = {item.price}
+        cost ={item.const}
       />
    })
 
